f55504b7eff4ab31f958faac3ed650f6a98cf946
Stephanie Smithson <stephaniesmithson16@gmail.com>
2022-11-16 14:50:17 -0600

    Initial Commit

---
+++ ink_skeleton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ink_skeleton
+//
+//  Created by Stephanie Smithson on 11/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ink_skeleton/ink_skeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ink_skeletonApp.swift
+//  ink_skeleton
+//
+//  Created by Stephanie Smithson on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ink_skeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
